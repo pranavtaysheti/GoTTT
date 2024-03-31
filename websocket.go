f2b4ad07cd11257ffb19187f102013d8b98a0848
Pranav Taysheti <pranavtaysheti@gmail.com>
2024-03-31 18:04:49 +0530

    Disassociate Websocket from client after connection drops

--- websocket.go
+++ websocket.go
@@ -20,31 +20,28 @@ const (
 )
 
 func wsHandler(ws *websocket.Conn) {
-	c, err := getClientCookie(ws.Request())
+	cl, err := getClientFromRequest(ws.Request())
 	if err != nil {
-		log.Println("Cookie not found. Could not associate websocket with a client.")
+		log.Println(err)
 		return
 	}
 
-	cl, err := getClientFromCookie(c)
+	err = cl.ConnectSocket(ws)
 	if err != nil {
-		log.Println("Client Object not found. Could not associate websocket with client")
+		log.Println(err)
 		return
 	}
 
-	cl.socket = ws
-	wsLoop(ws)
-}
-
-func wsLoop(ws *websocket.Conn) {
 	b := make([]byte, 1024)
-
-	var err error
+	
 	for err != io.EOF {
 		if err != nil {
 			log.Println("Error reading websocket:", err)
 		}
 		_, lerr := ws.Read(b)
 		err = lerr
-	} 
+	}
+
+	cl.socket = nil
 }
+
